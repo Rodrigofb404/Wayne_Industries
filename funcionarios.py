3ff35759c1cb38da1b00a483ce20f1c8b673d308
Rodrigo <rodrigof0501@gmail.com>
2024-10-01 19:07:26 -0300

    Add  "__str__"

--- funcionarios.py
+++ funcionarios.py
@@ -1,18 +1,40 @@
 class Funcionarios:
-    def __init__(self, id, nome, salario) -> None:
+    def __init__(self, id, nome, idade, salario) -> None:
         self.id = id
         self.nome = nome
         self.salario = salario
-        self.nivel_autorizacao = 5 # 5 -> 1 | 1 é o maior;
+        self.idade = idade
 
-class Gerentes(Funcionarios):
-    def __init__(self) -> None:
-        pass
+    def __str__(self):
+        return f"ID: {self.id} | Nome: {self.nome} | Salário: {self.salario:.2f}"
 
-class adm_seguranca(Funcionarios):
-    pass
 
-class diretor(Funcionarios):
-    def __init__(self, id, nome, salario) -> None:
-        super().__init__(id, nome, salario)
-    
+class Gerente(Funcionarios):
+    def __init__(self, id, nome, idade, salario) -> None:
+        super().__init__(id, nome, idade, salario)
+        self.nivel_autorizacao = 3  # Nível máximo de autorização
+
+    def __str__(self):
+        detalhes = super().__str__()
+        return f"{detalhes} | Nível de Autorização: {self.nivel_autorizacao}"
+
+
+class Adm_Seguranca(Funcionarios):
+    def __init__(self, id, nome, idade, salario) -> None:
+        super().__init__(id, nome, idade, salario)
+        self.nivel_autorizacao = 2  # Nível médio de autorização
+
+    def __str__(self):
+        detalhes = super().__str__()
+        return f"{detalhes} | Nível de Autorização: {self.nivel_autorizacao}"
+
+
+class Diretor(Funcionarios):
+    def __init__(self, id, nome, idade, salario) -> None:
+        super().__init__(id, nome, idade, salario)
+        self.nivel_autorizacao = 1  # Nível mais alto de autorização
+
+    def __str__(self):
+        detalhes = super().__str__()
+        return f"{detalhes} | Nível de Autorização: {self.nivel_autorizacao}"
+
